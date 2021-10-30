43c37210ae966b8fa7e6adae5aeaf42d616a01e1
Tavi <dragulceo@gmail.com>
2021-10-31 00:53:13 +0300

    Simple show/hide window and float window

--- Extensie/MainWindowController.m
+++ Extensie/MainWindowController.m
@@ -14,13 +14,16 @@
 @implementation MainWindowController
 
 - (void)hideWindow: (NSWindow *)window {
-    [window orderOut:nil];
+    //[window orderOut:nil];
+    [window setIsVisible:false];
 }
 
 - (void)showWindow: (NSWindow *)window {
-    [window makeKeyAndOrderFront:nil];
-    [window setLevel:NSStatusWindowLevel];
-    [NSApp activateIgnoringOtherApps:YES];
+    [window setIsVisible:true];
+    [self.window setLevel:NSFloatingWindowLevel];
+    //[window makeKeyAndOrderFront:nil];
+    //[window setLevel:NSStatusWindowLevel];
+    //[NSApp activateIgnoringOtherApps:YES];
 }
 
 - (void)attachToMousePosition {
@@ -29,13 +32,14 @@
     CGRect position = self.window.frame;
     position.origin.x = pos.x + 10;
     position.origin.y = pos.y - self.window.frame.size.height - 10;
-    if (pos.x > 1000 && pos.x < 1200 && pos.y > screen.frame.size.height - 30) {
+    float notchWidth = 200;
+    float notchHeight = 30;
+    if (pos.x > screen.frame.size.width / 2 - notchWidth / 2 && pos.x < screen.frame.size.width / 2 + notchWidth / 2 && pos.y > screen.frame.size.height - notchHeight) {
         [self.window setFrameOrigin: position.origin];
         [self showWindow: self.window];
     } else {
         [self hideWindow: self.window];
     }
-    //[self.window setFrameOrigin: position.origin];
 }
 
 
@@ -54,11 +58,6 @@
     }];
     [self hideWindow: self.window];
 }
-
-
-//
-
-
 
 
 @end
